9dc9085b49595a6ceb72d799f93cba7ade6d71ac
Dimitar Yurukov <mscalindt@protonmail.com>
2025-09-05 01:26:17 +0200

    Employ basic submodule update infrastructure
    Signed-off-by: Dimitar Yurukov <mscalindt@protonmail.com>

--- Makefile
+++ Makefile
@@ -3,3 +3,8 @@ rel:
 	@test -n "$(PRE)" || { echo 'PRE is empty'; exit 2; }
 	@test -n "$(CUR)" || { echo 'CUR is empty'; exit 2; }
 	sh ./rel.sh "$(REL)" "$(PRE)" "$(CUR)"
+
+sbm_up:
+	@test -n "$(SUB)" || { echo 'SUB is empty'; exit 2; }
+	@test -n "$(TAG)" || { echo 'TAG is empty'; exit 2; }
+	sh ./sbm_up.sh "$(SUB)" "$(TAG)"
---
+++ sbm_up.sh
@@ -0,0 +1,69 @@
+set -e
+
+case "${1:-x}" in
+    '-h' | '--help' | 'x')
+        printf "%s\n" '$1 = SUBMODULE RELATIVE GIT PATH'
+        printf "%s\n" '$2 = TAG'
+
+        exit 0
+    ;;
+esac
+
+[ "$#" -eq 2 ] || { echo "Expected exactly 2 arguments; got $#."; exit 2; }
+[ "$1" ] || { echo '$1/SUB is empty.'; exit 2; }
+[ "$2" ] || { echo '$2/TAG is empty.'; exit 2; }
+
+case "$1" in
+    *'shell-glossary'*) MOD_ALIAS='shell-glossary' ;;
+esac
+
+cd "$1"
+MAIN_DIR="$OLDPWD"
+git fetch
+COMMIT="$(git rev-list -n1 "$2")"
+LOG="$(git log --pretty=format:'%h %s' --no-decorate "HEAD..$COMMIT")"
+
+case "$MOD_ALIAS" in
+    'shell-glossary')
+        SRC_LOG=$(
+            printf "%s\n" "$LOG" | {
+                while IFS= read -r LINE; do
+                    case "$LINE" in
+                        *'(): [doc]'*)
+                            continue
+                        ;;
+                        *'(): '*)
+                            printf "%s\n" "$LINE"
+                        ;;
+                        *)
+                            continue
+                        ;;
+                    esac
+                done
+            }
+        )
+    ;;
+esac
+
+git checkout "$COMMIT"
+cd "$MAIN_DIR"
+git add .
+
+if [ "$LOG" ] && [ "$SRC_LOG" = "$LOG" ]; then
+    git commit -m \
+".gitmodules: $MOD_ALIAS -> \`$2\`
+
+- Complete log:
+$LOG" -Ss
+elif [ "$LOG" ]; then
+    git commit -m \
+".gitmodules: $MOD_ALIAS -> \`$2\`
+
+- Filtered log:
+$SRC_LOG
+
+- Complete log:
+$LOG" -Ss
+else
+    git commit -m ".gitmodules: $MOD_ALIAS -> \`$2\`" -Ss
+fi
